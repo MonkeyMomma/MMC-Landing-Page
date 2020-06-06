93d2af66137bc0444db0aa3028f9869bdcb5b8b4
Monkey Momma Creations <59214914+MonkeyMomma@users.noreply.github.com>
2020-06-06 00:10:51 -0700

    fix: nav styling

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -11,57 +11,167 @@
  *
  * JS Standard: ESlint
  *
-*/
+ */
 
 /**
  * Define Global Variables
-*/
-var sections = document.querySelectorAll('section');
-var menu_links = [];
-var menu_container = document.getElementById('navbar__list');
-for (var i = 0; i < sections.length; i++) {
-  var section_el = sections[i];
+ *
+ */
 
-  const a_tag = createMenuLink('#' + section_el.id, section_el.dataset.nav);
-  menu_links.push(a_tag);
+// sections is an HTMLCollection live and ordered, which does not include spaces.
+const sections = document.getElementsByTagName("section");
+const nav = document.querySelector("#navbar__list");
+/**
+ * End Global Variables
+ * Start Helper Functions
+ *
+ */
 
-  var menu_item = document.createElement('li');
-  menu_item.appendChild(a_tag);
-  menu_container.appendChild(menu_item);
+function insertNavLink(navLinkName, sectionId) {
+  // create a link
+  const htmlTextToAdd = `<li><a href="#${sectionId}" class="menu__link">${navLinkName}</a></li>`;
+  // add it to the nav
+  nav.insertAdjacentHTML("beforeend", htmlTextToAdd);
 }
 
-function createMenuLink(href, content) {
-  const el = document.createElement('a');
-  el.href = href;
-  el.textContent = content;
-  el.classList.add('menu__link');
-  return el;
-}
-
-var isInViewport = function (el) {
-  var bounding = el.getBoundingClientRect();
+function isTopSectionInViewport(el) {
+  // get the coordinates of the element box from the viewport
+  const rect = el.getBoundingClientRect();
+  // return true if the element top is near the top of the viewport, false otherwise
   return (
-    // bounding.top >= 0 &&
-    bounding.top <= (window.innerHeight || document.documentElement.clientHeight)/2
+    rect.top >= 0 &&
+    // 0.4 works well across devices without overlapping sections
+    rect.top <=
+      0.4 * (window.innerHeight || document.documentElement.clientHeight)
   );
-};
+}
 
-let activeSectionIndex = null;
-window.addEventListener('scroll', function (e) {
-  let newActiveSectionIndex = null;
-  for (var i = 0; i < sections.length; i++) {
-    if (isInViewport(sections[i])) {
-      newActiveSectionIndex = i;
+// simple and perfect helper function taken from https://muffinman.io/javascript-get-element-offset/
+function getElementOffset(el) {
+  // get the coordinates of the element box from the viewport
+  const rect = el.getBoundingClientRect();
+  // return element absolute top/left position as an object (element scroll in viewport + document scroll)
+  return {
+    top: rect.top + window.pageYOffset,
+    left: rect.left + window.pageXOffset
+  };
+}
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ *
+ */
+
+// build the nav
+function buildNav(sections) {
+  for (const section of sections) {
+    let navLinkName = section.getAttribute("data-nav");
+    let sectionId = section.getAttribute("id");
+    insertNavLink(navLinkName, sectionId);
+  }
+}
+
+// Add class 'active' to section when near top of viewport
+function setSectionIntoViewActive(sections) {
+  for (const section of sections) {
+    // detect the navigation link that matches the current section
+    const activeLink = document.querySelector(
+      `a[href="#${section.getAttribute("id")}"]`
+    );
+    // checks if the current section is near top of viewport
+    if (isTopSectionInViewport(section)) {
+      // if it is, highlight menu link and section with active styles
+      section.classList.add("active");
+      activeLink.classList.add("menu__link--active");
+    } else {
+      // if it is not, remove active styles
+      section.classList.remove("active");
+      activeLink.classList.remove("menu__link--active");
     }
   }
-  if (newActiveSectionIndex != activeSectionIndex) {
-    if (activeSectionIndex != null) {
-      sections[activeSectionIndex].classList.remove('active');
-      menu_links[activeSectionIndex].classList.remove('menu__link_scrolled');
-    }
-    sections[newActiveSectionIndex].classList.add('active');
-    menu_links[newActiveSectionIndex].classList.add('menu__link_scrolled');
+}
 
-    activeSectionIndex = newActiveSectionIndex;
+// Scroll to section anchor ID using smooth scrollTO event
+function smoothScroll(el) {
+  window.scrollTo({
+    //scroll to element and account for sticky header offset
+    top: getElementOffset(el).top - nav.offsetHeight,
+    left: getElementOffset(el).left,
+    behavior: "smooth"
+  });
+}
+
+// create a "Go to Top" hidden floating button
+function createBtnUp() {
+  const htmlTextToAdd = `<a href="#" class="bottom__link hide">To top</a>`;
+  document.body.insertAdjacentHTML("afterbegin", htmlTextToAdd);
+}
+
+function showBtnUp() {
+  // create and add the buttom to the DOM as hidden element
+  const btn = document.querySelector(".bottom__link");
+  // remove the button if absolute scrolling y is lower than browser window viewport
+  if (window.pageYOffset <= 0.6 * window.innerHeight) {
+    btn.classList.add("hide");
+  } else {
+    btn.classList.remove("hide");
+    // add event handler only if button is visible
+    btn.addEventListener("click", function(e) {
+      e.preventDefault();
+      // smooth scroll to top of page
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+      });
+    });
   }
-}, false);
+}
+
+// show navigation bar after delay is elapsed, when used with scroll events,
+// timer gets reset and until the user stops scrolling
+function hideNav(delay) {
+  var timer;
+  timer && clearTimeout(timer);
+  nav.classList.add("hide");
+  timer = setTimeout(function() {
+    nav.classList.remove("hide");
+  }, delay);
+}
+
+/**
+ * End Main Functions
+ * Begin Events
+ *
+ */
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Build menu and hidden Button up on page load
+  buildNav(sections);
+  createBtnUp();
+
+  // Scroll to section on link click
+  nav.addEventListener("click", function(e) {
+    if (e.target.nodeName === "A") {
+      e.preventDefault();
+      // getting the clicked link
+      const activeSection = document.querySelector(
+        `section[id = ${e.target.getAttribute("href").slice(1)}]`
+      );
+      // smooth scrolling to the clicked link
+      smoothScroll(activeSection);
+    }
+  });
+
+  // Set active session
+setTimeout(
+    window.addEventListener("scroll", function() {
+      setSectionIntoViewActive(sections);
+      hideNav(1000);
+      showBtnUp();
+    }),
+    // this is a delay to reduce the frequency of scrolling events fired
+    2000
+  );
+});
